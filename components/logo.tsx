faa2b7d172f6942e03651fceb10722747d975aad
Carmenia <carmenjacobs0003@gmail.com>
2026-05-28 16:55:03 +0100

    Test logo component rendering

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,25 +1,20 @@
-type LogoProps = {
-  className?: string;
-  showWordmark?: boolean;
-};
-
-export function Logo(_props: LogoProps = {}) {
+export function Logo() {
   return (
-    <div style={{
-      width: '220px',
-      height: '80px',
-      background: 'red',
-      color: 'white',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      fontSize: '32px',
-      fontWeight: 'bold',
-      zIndex: 999999
-    }}>
+    <div
+      style={{
+        width: '220px',
+        height: '80px',
+        background: 'red',
+        color: 'white',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: '32px',
+        fontWeight: 'bold',
+        zIndex: 999999,
+      }}
+    >
       TEST LOGO
     </div>
   )
 }
-
-export default Logo;
